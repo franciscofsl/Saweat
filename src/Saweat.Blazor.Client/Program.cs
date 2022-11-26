a0d540c0a50a9f104352f23c90c68b3bcdbe112f
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2022-11-26 18:58:31 +0100

    Initial structure

---
+++ src/Saweat.Blazor.Client/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var host = builder.Build();
+await host.RunAsync();
