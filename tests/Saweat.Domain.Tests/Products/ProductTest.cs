b66373625ecfbdf513ca42381fb53469498a972c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2022-12-01 00:53:43 +0100

    updated nugets

--- tests/Saweat.Domain.Tests/Products/ProductTest.cs
+++ tests/Saweat.Domain.Tests/Products/ProductTest.cs
@@ -5,13 +5,6 @@ using Xunit;
 namespace Saweat.Domain.Tests.Products;
 
 public class ProductTest
-{
-    [Fact]
-    public void Product_Set_Code()
-    {
-        var product = new Product();
-        var newCode = "New Code";
-        product.SetCode(newCode);
-        product.Code.Should().Be(newCode);
-    }
+{ 
+    
 }
